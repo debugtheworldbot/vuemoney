4ab7e534731da1305134faca2c3778ba7cdb742e
debugtheworldbot <istiancz@gmail.com>
2020-06-07 10:10:59 +0800

    layout of icon

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
-    <div class="nav">
-        <router-link to="/money">
+    <nav>
+        <router-link to="/money" class="item">
             <Icon name="money"/>
             记账</router-link>
-        <router-link to="/labels">
+        <router-link to="/labels" class="item">
             <Icon name="tag"/>
             标签</router-link>
-        <router-link to="/statistics">
+        <router-link to="/statistics" class="item">
             <Icon name="chart"/>
             统计</router-link>
-    </div>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -19,7 +19,22 @@
 </script>
 
 <style lang="scss" scoped>
-    .nav{
-        border: 1px solid red;
+    nav{
+        display: flex;
+        flex-direction: row;
+        box-shadow: 0 0 3px rgba(0,0,0,0.3);
+        font-size: 14px;
+        > .item{
+            padding: 2px 0;
+            width: 33.33%;
+            display: flex;
+            justify-content: center;
+            flex-direction: column;
+            align-items: center;
+            > .icon{
+                width: 32px;
+                height: 32px;
+            }
+        }
     }
 </style>
